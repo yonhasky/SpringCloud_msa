ab31f49df6c8b67aa2d462dd03ccdf033fb3c878
yonhasky <yonhasky@naver.com>
2022-02-19 14:17:36 +0900

    order-service responseOrder commit

--- order-service/src/main/java/com/example/orderService/vo/ResponseOrder.java
+++ order-service/src/main/java/com/example/orderService/vo/ResponseOrder.java
@@ -2,15 +2,17 @@ package com.example.orderService.vo;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseOrder {
 	private String productId;
 	private Integer qty;
 	private Integer unitPrice;
 	private Integer totalPrice;
-	private Integer stock;
 	private Date createAt;
 	private String orderId;
 }
